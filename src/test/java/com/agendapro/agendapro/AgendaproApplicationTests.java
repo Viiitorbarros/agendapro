5dba369fdf1fb6c73c53c50b8913881bfe78d0ce
Vitor Barros <vitornf1221@gmail.com>
2025-07-02 14:50:02 -0300

    Estuturação do projeto

---
+++ src/test/java/com/agendapro/agendapro/AgendaproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendapro.agendapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
