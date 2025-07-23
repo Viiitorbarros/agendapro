9899ff378ba26994fab75ab073fe7a700795a27e
Vitor Barros <vitorbarros.pgr@gmail.com>
2025-07-23 13:55:19 -0300

    Teste da Tela de login, alteração dos configs

--- src/main/java/com/agendapro/agendapro/config/SecurityConfig.java
+++ src/main/java/com/agendapro/agendapro/config/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
@@ -28,6 +30,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**").permitAll() // Libera o H2 Console
                         // Regras para Agendamento
+
+
+                        // PERMITE A PERGUNTA DE "PREFLIGHT" DO CORS
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
                         .requestMatchers(HttpMethod.GET, "/agendapro/agendamento/**").hasAnyRole("ADMIN", "PROFESSOR")
                         .requestMatchers(HttpMethod.POST, "/agendapro/agendamento/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/agendapro/agendamento/**").hasRole("ADMIN")
@@ -35,7 +42,7 @@ public class SecurityConfig {
                         // Todas as outras requisições precisam de autenticação
                         .anyRequest().authenticated()
                 )
-                // Usaremos autenticação HTTP Basic por enquanto, que é simples para testar APIs
+                // Usaremos autenticação HTTP Basic por enquanto;
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
@@ -60,6 +67,21 @@ public class SecurityConfig {
                 professor.setSenha(passwordEncoder.encode("prof123")); // Senha será codificada
                 professor.setRole("PROFESSOR");
                 usuarioRepository.save(professor);
+            }
+        };
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        // Altere o "*" para o endereço exato do seu Live Server
+                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
