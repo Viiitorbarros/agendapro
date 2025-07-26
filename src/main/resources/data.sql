11e4d92f3327ccc89f04e8f1c5688d734c5b64a9
Vitor Barros <vitorbarros.pgr@gmail.com>
2025-07-26 00:20:57 -0300

    Troca de bco h2 para MySql

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO agendamentos (nome, telefone, data_hora) VALUES ('João Silva', '11987654321', '2025-07-03 10:00:00');
-INSERT INTO agendamentos (nome, telefone, data_hora) VALUES ('Maria Souza', '21998765432', '2025-07-03 14:30:00');
-INSERT INTO agendamentos (nome, telefone, data_hora) VALUES ('Carlos Lima', '31976543210', '2025-07-04 09:00:00');
