76b785614a95b1b5af085dbcb894401949d7d96d
Vitor Barros <vitorbarros.pgr@gmail.com>
2025-07-28 12:16:27 -0300

    Deploy

--- src/main/java/com/agendapro/agendapro/config/SecurityConfig.java
+++ src/main/java/com/agendapro/agendapro/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         // Altere o "*" para o endereço exato do seu Live Server
-                        .allowedOrigins("https://agendapro.fly.dev")
+                        .allowedOrigins("https://agendapro-frontend.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
